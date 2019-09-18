30904b8085cb574698de0ece316b2e0d21344635
root <root@localhost.localdomain>
2019-09-18 01:07:14 -0400

    updated stdin reading and added exit functionality

--- ash.c
+++ ash.c
@@ -6,9 +6,33 @@
 
 int main(int argc, char *argv[]) {
 	
-	char str[100];
-	
+	char *str;
+	size_t bufsize = 200;
+	size_t chars;
+
+	str = (char *)malloc(bufsize * sizeof(char));
+
 	while(strcmp(str, "exit") != 0){
+		
+		printf("%s", "ash>> ");
+		chars = getline(&str, &bufsize, stdin);
+		
+		char *pstr = strtok(str, " ");
+		
+		if(strcmp(pstr, "exit\n") == 0)
+			exit(0);
+
+		while(pstr != NULL){
+		
+			printf("%s ", pstr);
+			pstr = strtok(NULL, " ");
+
+		}
+
+		//handles exit functionality
+		if(strcmp(str, "exit") == 0)
+			exit(0);
+
 		int rc = fork();
 		if (rc < 0) {
 			//fork failed
@@ -16,11 +40,8 @@ int main(int argc, char *argv[]) {
 			exit(1);
 		} else if(rc == 0){
 
-			printf("ash>> ");
-			scanf("%s", str);
-			
-			char *myargs[3];
-			myargs[0] = strdup("exit");
+			char *myargs[2];
+			myargs[0] = strdup(str);
 			myargs[1] = NULL;
 			execvp(myargs[0], myargs);
 
