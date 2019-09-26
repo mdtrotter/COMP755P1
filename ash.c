0610e2d5095a4e3d19d8520f01a3e76dfbe9a750
root <root@localhost.localdomain>
2019-09-26 18:15:24 -0400

    added comments and removed debug code

--- ash.c
+++ ash.c
@@ -6,37 +6,48 @@
 
 int main(int argc, char *argv[]) {
 	
+	//stores input string
 	char *str;
 	size_t bufsize = 200;
 	size_t chars;
 	str = (char *)malloc(bufsize * sizeof(char));
 
+	//stores path variables
 	char *pathArr[20];
 	pathArr[0] = "/usr/bin/";
 	pathArr[1] = "/bin/";
 	int pathctr = 2;
 	
+	//stores path variable to be executed
 	char buff[255];
 
 	while(strcmp(str, "exit") != 0){
+		
+		//begins shell prompt
 		printf("%s", "ash>> ");
 		chars = getline(&str, &bufsize, stdin); 
-
+		
+		// splits input string by " " and "\n"
 		char *pstr = strtok(str, " \n");
+
+		//exit function
 		if(strcmp(pstr, "exit") == 0){
 			exit(0);
 		}
+
+		//cd function
 		else if(strcmp(pstr, "cd") == 0){
 
 			pstr = strtok(NULL, " \n");
-			printf("chdir %s", pstr);
 
 			chdir(pstr);
-
+		
+		//path function
 		}else if(strcmp(pstr, "path") == 0){
 			pstr = strtok(NULL, " \n");
 			while(pstr != NULL){
 				
+				//for each argument, stores it into pathArr array
 				pathArr[pathctr] = pstr;
 				pathctr++;
 				pstr = strtok(NULL, " \n");
@@ -50,19 +61,16 @@ int main(int argc, char *argv[]) {
 			exit(1);
 		}else if(rc == 0){
 			
+			//loops through pathArr and checks if shell command is in one of the saved paths
 			int pc = 0;
 			strcpy(buff, pathArr[pc]);
-			printf("%s", strcat(buff, pstr));
 			while(access(strcat(buff, pstr), X_OK) != 0 && pc < pathctr){
 				memset(buff, 0, sizeof buff);
 				pc++;
 				strcpy(buff, pathArr[pc]);
-				printf("%d", pc);
 			}
-			printf("%d", pc);
 			if (pc < pathctr){
 				char *myargs[2];
-				printf("%s", buff);
 				myargs[0] = strdup(buff);
 				myargs[1] = NULL;
 				execvp(myargs[0], myargs);
