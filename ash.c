07c70b8f6700ebe3d8e46caa8a93197301120803
root <root@localhost.localdomain>
2019-09-27 12:13:52 -0400

    added support for commands with multiple arguments

--- ash.c
+++ ash.c
@@ -61,19 +61,46 @@ int main(int argc, char *argv[]) {
 			exit(1);
 		}else if(rc == 0){
 			
-			//loops through pathArr and checks if shell command is in one of the saved paths
 			int pc = 0;
+			char *myargs[10];
+			//loops through pathArr and checks if shell command is in one of the saved paths
+			memset(buff, 0, sizeof buff);
 			strcpy(buff, pathArr[pc]);
-			while(access(strcat(buff, pstr), X_OK) != 0 && pc < pathctr){
-				memset(buff, 0, sizeof buff);
+			char comm[255] = "";
+			//char *comm;
+			//comm = (char *)malloc(bufsize * sizeof(char *));
+			int init = 1;
+			int argC = 1;
+
+			while(pstr != NULL){
+				if(init == 1){
+					strcat(comm, pstr);
+					init = 0;
+				}else{
+					myargs[argC] = pstr;
+					argC++;
+				}
+				pstr = strtok(NULL, " \n");
+			
+			}
+			//comm[(sizeof(comm) - 1)] = '\0';
+
+			printf("%s\n", comm);
+			printf("%s\n", myargs[1]);
+			while(access(strcat(buff, comm), X_OK) != 0 && pc < pathctr){
+				printf("%d\n", pc);
 				pc++;
+				memset(buff, 0, sizeof buff);
 				strcpy(buff, pathArr[pc]);
 			}
+
+			printf("%s\n", buff);
+			printf("%d\n", argC);
 			if (pc < pathctr){
-				char *myargs[2];
+				//char *myargs[2];
 				myargs[0] = strdup(buff);
-				myargs[1] = NULL;
-				execvp(myargs[0], myargs);
+				myargs[argC] = NULL;
+				execv(myargs[0], myargs);
 			}
 		}else{
 			int rc_wait = wait(NULL);
