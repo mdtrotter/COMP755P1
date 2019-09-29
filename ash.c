31937b1ce997142d055cdcebe11532f1bac85ec3
root <root@localhost.localdomain>
2019-09-29 16:35:37 -0400

    finished &, |, > functions (TODO: trim white space, batch mode)

--- ash.c
+++ ash.c
@@ -21,72 +21,93 @@ int main(int argc, char *argv[]) {
 	//stores path variable to be executed
 	char buff[255];
 
+	//stores counter for commands executed already
+	int execCtr=0;
+
 	while(strcmp(str, "exit") != 0){
 		
 		//begins shell prompt
 		printf("%s", "ash>> ");
 		chars = getline(&str, &bufsize, stdin); 
-		
-		// splits input string by " " and "\n"
-		char *pstr = strtok(str, " \n");
-
-		//exit function
-		if(strcmp(pstr, "exit") == 0){
-			exit(0);
-		}
-
-		//cd function
-		else if(strcmp(pstr, "cd") == 0){
-
-			pstr = strtok(NULL, " \n");
-
-			chdir(pstr);
-		
-		//path function
-		}else if(strcmp(pstr, "path") == 0){
-			pstr = strtok(NULL, " \n");
-			while(pstr != NULL){
-				
-				//for each argument, stores it into pathArr array
-				pathArr[pathctr] = pstr;
-				pathctr++;
-				pstr = strtok(NULL, " \n");
-			}
+		if(strchr(str, '|') != NULL || strchr(str, '>') != NULL){
+			system(str);
 		}else{
-	
-		int rc = fork();
-		if (rc < 0) {
-			//fork failed
-			fprintf(stderr, "fork failed\n");
-			exit(1);
-		}else if(rc == 0){
+			// splits input string by " " and "\n"
+			char *pstr = strtok(str, " \n");
+
+			//exit function
+			if(strcmp(pstr, "exit") == 0){
+			exit(0);
+
+			//cd function
+			}else if(strcmp(pstr, "cd") == 0){
+
+				pstr = strtok(NULL, " \n");
+
+				chdir(pstr);
+		
+			//path function
+			}else if(strcmp(pstr, "path") == 0){
+				pstr = strtok(NULL, " \n");
+				while(pstr != NULL){
+				
+					//for each argument, stores it into pathArr array
+					pathArr[pathctr] = pstr;
+					pathctr++;
+					pstr = strtok(NULL, " \n");
+				}
 			
+			}else{
+		while(pstr != NULL){
+						
+
+			/*int rc = fork();
+			if (rc < 0) {
+				//fork failed
+				fprintf(stderr, "fork failed\n");
+				exit(1);
+			}else if(rc == 0){
+			*/
 			int pc = 0;
 			char *myargs[10];
 			//loops through pathArr and checks if shell command is in one of the saved paths
+			
+			//clears buff array
 			memset(buff, 0, sizeof buff);
-			strcpy(buff, pathArr[pc]);
-			char comm[255] = "";
-			//char *comm;
-			//comm = (char *)malloc(bufsize * sizeof(char *));
-			int init = 1;
-			int argC = 1;
 
+			//copy first path in pathArr to buff
+			strcpy(buff, pathArr[pc]);
+
+			//stores commands
+			char comm[255] = "";
+
+			//init is a boolean value that is set to false once the first argument is saved to comm
+			int init = 1;
+
+			//argC tracks each argument associated with the first command
+			int argC = 1;
+			
+			//loops through command string and places command at the beginning and args into myargs array
 			while(pstr != NULL){
+				//if & is found, then execute command up to & and save execCtr to start after & in next loop through
+				if(strcmp(pstr, "&") == 0){
+					pstr = strtok(NULL, " \n");
+					break;
+				}
 				if(init == 1){
 					strcat(comm, pstr);
 					init = 0;
-				}else{
+					execCtr++;
+				}else{	
 					myargs[argC] = pstr;
 					argC++;
+					execCtr++;
 				}
 				pstr = strtok(NULL, " \n");
-			
+				printf("pstr is now %s\n", pstr);
 			}
-			//comm[(sizeof(comm) - 1)] = '\0';
-
+			
 			printf("%s\n", comm);
-			printf("%s\n", myargs[1]);
 			while(access(strcat(buff, comm), X_OK) != 0 && pc < pathctr){
 				printf("%d\n", pc);
 				pc++;
@@ -96,18 +117,36 @@ int main(int argc, char *argv[]) {
 
 			printf("%s\n", buff);
 			printf("%d\n", argC);
-			if (pc < pathctr){
-				//char *myargs[2];
-				myargs[0] = strdup(buff);
-				myargs[argC] = NULL;
-				execv(myargs[0], myargs);
+			
+			int rc = fork();
+			if (rc < 0){
+				//fork failed
+				fprintf(stderr, "fork failed\n");
+				exit(1);
 			}
-		}else{
-			int rc_wait = wait(NULL);
-		}
-		}
 
+			if (rc == 0){
+
+
+			//if a path was found for the current argument
+				if (pc < pathctr){
+					//char *myargs[2];
+					myargs[0] = strdup(buff);
+					myargs[argC] = NULL;
+					if(execvp(myargs[0], myargs) == -1){
+						char error_message[30] = "error\n";
+						write(STDERR_FILENO, error_message, strlen(error_message));
+					}
+					exit(1);
+				}
+				exit(1);
+			
+			}else{
+				int rc_wait = wait(NULL);
+			}
+		}
+		}
+	}
 	}
 	return 0;
-
 }
