47498f293b588cdaaf6a8181f37638b88835f50f
root <root@localhost.localdomain>
2019-10-05 09:45:47 -0400

    fully implemented pipe (todo: parse whitespace, implement redirect, possibly check path for each pipe command (currently just checks first cmd path))

--- ash.c
+++ ash.c
@@ -42,35 +42,12 @@ int main(int argc, char *argv[]) {
 		//begins shell prompt
 		printf("%s", "ash>> ");
 		chars = getline(&str, &bufsize, stdin); 
-		if(/*strchr(str, '|') != NULL ||*/ strchr(str, '>') != NULL){
+		if( strchr(str, '>') != NULL){
 			system(str);
 		}else{
 			// splits input string by " " and "\n"
 			char *pstr = strtok(str, " \n");
-
-			//exit function
-			/*if(strcmp(pstr, "exit") == 0){
-				exit(0);
-
-			//cd function
-			}else if(strcmp(pstr, "cd") == 0){
-
-				pstr = strtok(NULL, " \n");
-
-				chdir(pstr);
-		
-			//path function
-			}else if(strcmp(pstr, "path") == 0){
-				pstr = strtok(NULL, " \n");
-				while(pstr != NULL){
-				
-					//for each argument, stores it into pathArr array
-					pathArr[pathctr] = pstr;
-					pathctr++;
-					pstr = strtok(NULL, " \n");
-				}
 			
-			}else{*/
 		while(pstr != NULL){
 						
 
@@ -84,8 +61,18 @@ int main(int argc, char *argv[]) {
 
 			//tracks how many paths have been tested with the command
 			int pc = 0;
-			char *myargs[100];
-			int argInit[10];
+			char **args[5];
+			char *myargs[10];
+			char *myargs2[10];
+			char *myargs3[10];
+			char *myargs4[10];
+			char *myargs5[10];
+
+			args[0] = myargs;
+			args[1] = myargs2;
+			args[2] = myargs3;
+			args[3] = myargs4;
+			args[4] = myargs5;
 
 			//clears buff array
 			memset(buff, 0, sizeof buff);
@@ -101,6 +88,7 @@ int main(int argc, char *argv[]) {
 
 			//argC tracks each argument associated with the first command
 			int argC = 1;
+			int arrC = 0;
 			
 			//will become 1 if a pre-built command is issued and will then skip the fork
 			int pb = 0;
@@ -108,11 +96,11 @@ int main(int argc, char *argv[]) {
 			//pipe stuff
 			int numPipes = 0;
 			int isPipe = 0;
-			int fd[2];
+			/*int fd[2];
 			if(pipe(fd)){
 				perror("pipe");
 				exit(1);
-			}
+			}*/
 			
 			//loops through command string and places command at the beginning and args into myargs array
 			while(pstr != NULL){
@@ -124,27 +112,26 @@ int main(int argc, char *argv[]) {
 					break;
 				}
 				else if(strcmp(pstr, "|") == 0){
-					myargs[argC] = NULL;
-					argC++;
+					args[arrC][argC] = NULL;
+					arrC++;
+					argC=0;
 
 					numPipes++;
-					argInit[numPipes] = argC;
 					pstr = strtok(NULL, " \n");
 					isPipe = 1;
 					while(pstr != NULL && strcmp(pstr, "&") != 0 && strcmp(pstr, ">") != 0){
 						if(strcmp(pstr, "|") == 0){
-							myargs[argC] = NULL;
-							argC++;
+							args[arrC][argC] = NULL;
+							arrC++;
+							argC=0;
 							numPipes++;
-							argInit[numPipes] = argC;
 							pstr = strtok(NULL, " \n");
 						}
-						myargs[argC] = pstr;
+						args[arrC][argC] = pstr;
 						argC++;
 						pstr = strtok(NULL, " \n");
 					}
-					myargs[argC] = NULL;
-					argC++;
+					args[arrC][argC] = NULL;
 				}
 				else if(strcmp(pstr, "exit") == 0){
 					exit(0);
@@ -166,11 +153,10 @@ int main(int argc, char *argv[]) {
 				else if(init == 1){
 					strcat(comm, pstr);
 					init = 0;
-				}else{	
-					myargs[argC] = pstr;
+					args[arrC][argC] = NULL;
+				}else{
+					args[arrC][argC] = pstr;
 					argC++;
-				}
-				if(myargs[argC-1] != NULL){
 					myargs[argC] = NULL;
 				}
 				pstr = strtok(NULL, " \n");
@@ -182,6 +168,7 @@ int main(int argc, char *argv[]) {
 				memset(buff, 0, sizeof buff);
 				strcpy(buff, pathArr[pc]);
 			}
+			args[0][0] = strdup(buff);
 			
 			int rc = fork();
 			if (rc < 0){
@@ -195,29 +182,46 @@ int main(int argc, char *argv[]) {
 
 			//if a path was found for the current argument
 				if (pc < pathctr && pb == 0 && isPipe == 0){
-					myargs[0] = strdup(buff);
-					//myargs[argC] = NULL;
-					if(execvp(myargs[0], myargs) == -1){
+					//args[0][0] = strdup(buff);
+					if(execvp(args[0][0], args[0]) == -1){
 						char error_message[30] = "error\n";
-						write(STDERR_FILENO, error_message, strlen(error_message));
-						exit(1);
-					}
-				}else if(isPipe == 1){
-					dup2(fd[1], 1);
-					close(fd[0]);
-					close(fd[1]);
-					myargs[0] = strdup(buff);
-					if(execvp(myargs[0], myargs) == -1){
-						char error_message[30] = "error1\n";
 						write(STDERR_FILENO, error_message, strlen(error_message));
 						exit(1);
 					}
 				}
 				exit(1);
-			}else{
-				if(pc < pathctr && isPipe == 1){
-				
-					rc = fork();
+			}else{ 
+				if(pc < pathctr && isPipe == 1 && pb == 0){
+					int fd[2];
+					int fdd = 0;
+					//int *fds[10][2];
+					for(int i=0; i<numPipes+1; i++){
+						
+						if(i != numPipes){
+							pipe(fd);
+						}
+						if((rc = fork()) == -1){
+							perror("fork");
+							exit(1);
+						}else if(rc == 0){
+							dup2(fdd, 0);
+							if(i != numPipes){
+								dup2(fd[1], 1);
+							}
+							close(fd[0]);
+							if(execvp(args[i][0], args[i]) == -1){
+								char error_message[30] = "error1\n";
+								write(STDERR_FILENO, error_message, strlen(error_message));
+								exit(1);
+							}
+						}else{
+							int rc_wait = wait(NULL);
+							//close(fd[0]);
+							close(fd[1]);
+							fdd = fd[0];
+						}
+					}
+					/*rc = fork();
 
 					if(rc < 0){
 						fprintf(stderr, "fork failed\n");
@@ -227,7 +231,7 @@ int main(int argc, char *argv[]) {
 						dup2(fd[0], 0);
 						close(fd[1]);
 						close(fd[0]);
-						if(execvp(myargs[argInit[1]], myargs) == -1){
+						if(execvp(args[1][0], args[1]) == -1){
 							char error_message[30] = "error2\n";
 							write(STDERR_FILENO, error_message, strlen(error_message));
 							exit(1);
@@ -237,15 +241,14 @@ int main(int argc, char *argv[]) {
 						close(fd[1]);
 						int rc_wait = wait(NULL);
 					}
-				
-				}else{
+				*/
+				}/*else{
 					int rc_wait = wait(NULL);
-				}
+				}*/
 			}
 			int rc_wait = wait(NULL);
 		}
 		}
 	}
-	
 	return 0;
 }
